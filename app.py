b83d754e11488821168ce48b5f04b4e28808f9b4
Joshua Oladipo <joshuaoladipo2005@gmail.com>
2026-07-30 12:47:44 +0100

    Handle missing global market data safely

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def home():
 
     coins = get_market_data()
-    global_data = get_global_data()
+    global_data = get_global_data() or {}
 
     return render_template(
         "index.html",
